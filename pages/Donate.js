26f2ab28b9e8a098838f674313b2727ced634799
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2022-07-29 14:17:11 +0800

    whoops forgor to add a file

---
+++ pages/Donate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Donate() {
+  return (
+    <div>
+      <Header tab="Donate" />
+    </div>
+  );
+}
+
+export default Donate;
