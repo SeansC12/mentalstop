1342990fed8d30986f098093ff4cecb2b9d2328a
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2022-08-10 09:50:38 +0800

    finished up get help starting page

--- components/GetHelp/GetHelpCard.js
+++ components/GetHelp/GetHelpCard.js
@@ -2,23 +2,21 @@ import React from "react";
 import Image from "next/image";
 
 function GetHelpCard({ data, index }) {
-  const { Header, Copy, ButtonText } = data;
+  let { Header, Copy, ButtonText, ImageLink } = data;
+  ImageLink =
+    "https://images.unsplash.com/photo-1554757387-2a28855c78fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80";
   const colours = ["#DED4D4", "#E2DB98", "#AFE9AA", "#C6DAF9"];
   return (
     <div
       className={`relative flex flex-col text-left bg-slate-500 rounded-2xl font-Inter overflow-hidden max-h-56`}
     >
-      <img
-        src="https://images.unsplash.com/photo-1554757387-2a28855c78fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
-        className="w-full h-full"
-      />
+      <img src={ImageLink} className="object-cover" />
       <div className="absolute z-10 flex flex-col gap-3 m-8">
-        <h1 className="font-normal text-4xl">{Header}</h1>
-        <h2 className="font-medium text-xl">{Copy}</h2>
+        <h1 className="font-normal font-lora text-4xl">{Header}</h1>
+        <h2 className="font-medium font-Inter text-xl">{Copy}</h2>
         <button
-          className={`bg-[${
-            colours[((index + 1) % 4) - 1]
-          }] rounded-2xl px-4 py-2 w-max`}
+          className={`rounded-2xl px-4 py-2 w-max`}
+          style={{ backgroundColor: colours[((index + 1) % 4) - 1] }}
         >
           {ButtonText}
         </button>
--- pages/GetHelp.js
+++ pages/GetHelp.js
@@ -17,6 +17,20 @@ const ourOptions = {
   },
 };
 
+const mentalHealthHabits = {
+  Header: "Learn more about positive mental health practices",
+  Copy: "We have curated a list of the most effective positive mental health practices that you can do every day to help improve your mental health.",
+  ButtonText: "Learn More",
+  ImageLink: "",
+};
+
+const professionalSupport = {
+  Header: "Get professional support.",
+  Copy: "If none of the self-help techniques work for you, remember that professional help is always one call away from you.",
+  ButtonText: "Find Support",
+  ImageLink: "",
+};
+
 const optionsArray = [];
 let ourOptionsComponentArray = [];
 
@@ -45,14 +59,121 @@ function GetHelp() {
           <p className="mt-3">- Albert Dumbledore</p>
         </div>
         <div>
+          {/* Professional Support */}
+          {/* Header */}
+          <div className="font-Inter text-center text-md mt-10">Seek Help</div>
+          {/* Line */}
+          <div className="grid place-items-center my-3">
+            <div className="border border-[#1A8C10] w-5 rounded" />
+          </div>
+          {/* Subheader */}
+          <div className="font-lora text-center text-2xl mb-10 -mt-2">
+            Professional Help
+          </div>
+          <div className="grid place-items-center my-3">
+            <div className="border border-[#1A8C10] w-[75%] rounded" />
+          </div>
+
+          <div className="mt-12 font-lora text-center text-3xl">
+            There is nothing to be <strong>ashamed</strong> about reaching out
+            for help. Things get crazy and overwhelming, it's life.
+          </div>
+          <div className="mt-10">
+            <GetHelpCard data={professionalSupport} index={0} />
+          </div>
+        </div>
+
+        <div>
+          {/* Good Mental Health Practices */}
+          {/* Header */}
+          <div className="font-Inter text-center text-md mt-20">Seek Help</div>
+
+          {/* Line */}
+          <div className="grid place-items-center my-3">
+            <div className="border border-[#1A8C10] w-5 rounded" />
+          </div>
+
+          {/* Subheader */}
+          <div className="font-lora text-center text-2xl mb-10 -mt-2">
+            Positive Mental Health Practices
+          </div>
+
+          <div className="grid place-items-center my-3">
+            <div className="border border-[#1A8C10] w-[75%] rounded" />
+          </div>
+
+          <div className="mt-12 font-lora text-center text-3xl">
+            Mental Health is also about <strong>protecting</strong> yourself and
+            <strong> maintaining</strong> a healthy mental lifestyle
+          </div>
+          <div className="mx-[12.5%]">
+            <div className="font-Inter text-center text-md mt-10">
+              We experience varying emotions, ranging from happiness to sadness
+              to anger and more. On some days, we may feel energised while on
+              other days, we may feel stressed. It is normal to feel all kinds
+              of emotions because mental well-being is about accepting and
+              embracing all these emotions.
+            </div>
+
+            <div className="font-Inter text-center text-md mt-8">
+              <strong>It’s okay not to be okay at times.</strong> We all face
+              different challenges daily, sometimes it can be tiring and
+              overwhelming. Hence, it is paramount that we understand how to
+              cope with our emotions and events happening around us.
+            </div>
+          </div>
+
+          <div className="my-10 grid place-items-center">
+            <div className="border border-[#1A8C10] rounded w-[75%]"></div>
+          </div>
+
+          <div className="mb-5 font-lora text-center text-3xl">
+            Taking care of our <strong>mental wellbeing</strong>
+          </div>
+
+          <div className="flex items-center justify-center">
+            <iframe
+              src="https://www.youtube.com/embed/WeY79OCq9bI"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+              className="w-full max-w-full md:max-w-[70%] md:w-[70%] aspect-video"
+            ></iframe>
+          </div>
+          <div className="mt-12 mb-5 font-lora text-center text-xl">
+            Sustaining mental-wellbeing requires{" "}
+            <strong>time and effort. </strong>Click the card below and start
+            adopting positive mental health habits!
+          </div>
+          <GetHelpCard data={mentalHealthHabits} index={2} />
+        </div>
+
+        <div className="my-10 grid place-items-center">
+          <div className="border border-[#1A8C10] rounded w-[75%]"></div>
+        </div>
+
+        <div>
+          {" "}
           {/* Self-Assessment */}
+          {/* Header */}
+          <div className="font-Inter text-center text-md mt-12">
+            Self-Assessment
+          </div>
+          {/* Line */}
+          <div className="grid place-items-center my-3">
+            <div className="border border-[#1A8C10] w-5 rounded" />
+          </div>
+          {/* Subheader */}
+          <div className="font-lora text-center text-2xl mb-10 -mt-2">
+            Explore our options
+          </div>
           {ourOptionsComponentArray.map((component, key) => (
             <div className="mb-8" key={key}>
               {component}
             </div>
           ))}
         </div>
-        <div>{/* Professional Support */}</div>
       </main>
     </div>
   );
