66f7343e78b25a9fbcc1fd1c4bb7322079a26cc0
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-08-13 20:08:35 +0800

    questionnares cards set up, styling is non existent, still buggy

---
+++ pages/GetHelp/Questions.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import Header from "../../components/Header/Header";
+import questions from "../../public/questions.json";
+// import { SliderWhite } from "../../components/GetHelp/Slider";
+
+let questionScores = 0;
+export default function Questions() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [mcqInput, setMcqInput] = useState(null);
+  let input = 5;
+
+  const handleSubmit = () => {
+    questionScores +=
+      questions[currentQuestionIndex].answersToPoints[input] ||
+      questions[currentQuestionIndex].answersToPoints[mcqInput];
+
+    if (currentQuestionIndex == questions.length - 1) {
+    } else {
+      setCurrentQuestionIndex((i) => i + 1);
+    }
+    console.log(questions[currentQuestionIndex].answersToPoints[mcqInput]);
+  };
+
+  return (
+    <div>
+      {/* <Header tab="Get Help" /> */}
+      <main>
+        <div className="flex">
+          <div className="">
+            <p>{questions[currentQuestionIndex].question}</p>
+            <p>{questions[currentQuestionIndex].subtext}</p>
+            {questions[currentQuestionIndex].type == "range" ? (
+              <input
+                type="range"
+                onChange={(val) => {
+                  input = val;
+                }}
+                min={1}
+                max={10}
+              ></input>
+            ) : (
+              Object.entries(
+                questions[currentQuestionIndex].answersToPoints
+              ).map((answer) => (
+                <button
+                  onClick={() => {
+                    setMcqInput(answer);
+                  }}
+                  style={{
+                    backgroundColor: mcqInput == answer ? "blue" : "white",
+                  }}
+                >
+                  {answer}
+                </button>
+              ))
+            )}
+            <button onClick={handleSubmit}>Submit</button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
