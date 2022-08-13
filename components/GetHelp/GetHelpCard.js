13a75ca12aa400335ac2bdd29783dc2324f40871
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-08-13 20:08:13 +0800

    questionnares cards set up, styling is non existent, still buggy

--- components/GetHelp/GetHelpCard.js
+++ components/GetHelp/GetHelpCard.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Router from "next/router";
 
 function GetHelpCard({ data, index, link }) {
-  let { Header, Copy, ButtonText, ImageLink } = data;
+  let { Header, Copy, ButtonText, ImageLink, redirectLink } = data;
   ImageLink =
     "https://images.unsplash.com/photo-1554757387-2a28855c78fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80";
   const colours = ["#DED4D4", "#E2DB98", "#AFE9AA", "#C6DAF9"];
@@ -13,14 +14,15 @@ function GetHelpCard({ data, index, link }) {
       <div className="absolute z-10 flex flex-col gap-3 m-8">
         <h1 className="font-normal font-lora text-4xl">{Header}</h1>
         <h2 className="font-medium font-Inter text-xl">{Copy}</h2>
-        <a href={link ?? link}>
-          <button
-            className={`rounded-2xl px-4 py-2 w-max`}
-            style={{ backgroundColor: colours[((index + 1) % 4) - 1] }}
-          >
-            {ButtonText}
-          </button>
-        </a>
+        <button
+          onClick={() => {
+            Router.push(`${redirectLink}`)
+          }}
+          className={`rounded-2xl px-4 py-2 w-max`}
+          style={{ backgroundColor: colours[((index + 1) % 4) - 1] }}
+        >
+          {ButtonText}
+        </button>
       </div>
     </div>
   );
---
+++ components/GetHelp/Slider.js
@@ -0,0 +1,27 @@
+import { createStyles, Slider } from '@mantine/core';
+import { IconGripHorizontal } from '@tabler/icons';
+
+const useStyles = createStyles((theme) => ({
+  thumb: {
+    border: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[3]
+    }`,
+    width: 28,
+    height: 22,
+    color: theme.colors.gray[5],
+    backgroundColor: theme.white,
+    borderRadius: theme.radius.sm,
+  },
+}));
+
+export function SliderWhite() {
+  const { classes } = useStyles();
+  return (
+    <Slider
+      classNames={classes}
+      thumbChildren={<IconGripHorizontal size={18} stroke={1.5} />}
+      defaultValue={40}
+      label={null}
+    />
+  );
+}
