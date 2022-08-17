ad77b73c74d24fbfcb65b8311778cd6c096529e0
Xuan Han <tan_xuan_han@s2020.ssts.edu.sg>
2022-08-17 22:24:13 +0800

    Added generating points for line of best fit from a series of x and y values

--- utils/questions.js
+++ utils/questions.js
@@ -1,49 +1,55 @@
-// code taken from https://stackoverflow.com/questions/11796810/calculate-trendline-and-predict-future-results
+function getBestFit(x, y) {
+  // This function only generates gradient and y-intercept, see getLineOfBestFit for actual points
+  const dataArrLength = x.length
 
-function LineFitter() {
-  this.count = 0;
-  this.sumX = 0;
-  this.sumX2 = 0;
-  this.sumXY = 0;
-  this.sumY = 0;
-}
-
-LineFitter.prototype = {
-  'add': function (x, y) {
-    this.count++;
-    this.sumX += x;
-    this.sumX2 += x * x;
-    this.sumXY += x * y;
-    this.sumY += y;
-  },
-  'project': function (x) {
-    var det = this.count * this.sumX2 - this.sumX * this.sumX;
-    var offset = (this.sumX2 * this.sumY - this.sumX * this.sumXY) / det;
-    var scale = (this.count * this.sumXY - this.sumX * this.sumY) / det;
-    return offset + x * scale;
+  // Get mean of x and y values of all points
+  let sumX = 0
+  let meanX = 0
+  let sumY = 0
+  let meanY = 0
+  for (let i = 0; i < dataArrLength; i++) {
+      sumX += x[i]
+      sumY += y[i]
   }
-};
+  meanX = sumX / dataArrLength
+  meanY = sumY / dataArrLength
 
-function linearProject(data, x) {
-  var fitter = new LineFitter();
-  for (var i = 0; i < data.length; i++) {
-    fitter.add(i, data[i]);
+  // Get total deviation for x and y values from mean and prepare for finding gradient
+  let totalDevXSquare = 0
+  let totalDevXY = 0
+  for (let i = 0; i < dataArrLength; i++) {
+      const eachX = x[i]
+      const eachY = y[i]
+      totalDevXSquare += Math.pow((eachX - meanX), 2)
+      totalDevXY += (eachX - meanX) * (eachY - meanY)
   }
-  return fitter.project(x);
+
+  // Get gradient
+  const gradient = totalDevXY / totalDevXSquare
+
+  // Get y-intercept
+  const interceptY = meanY - gradient * meanX
+
+  return [gradient, interceptY]
 }
 
-function isAboveProjected(data, useLastN, score) {
-  // remove everything in data except the useLastN most recent items
-  var dataToProject = data.slice(data.length - useLastN);
-  return score > linearProject(dataToProject, dataToProject.count + 1);
+function getLineOfBestFit(x, y) {
+  // This function only returns y values -- x values will stay the same
+  const dataArrLength = x.length
+  const _a = getBestFit(x, y), gradient = _a[0], interceptY = _a[1]
+
+  // Generate linear regression points
+  let bestFitY = []
+  for (let i = 0; i < dataArrLength; i++) {
+      const eachX = x[i]
+      const newY = gradient * eachX + interceptY
+      bestFitY.push(newY)
+  }
+  
+  return bestFitY
 }
 
-function isPositiveChange(data, useLastN) {
-  // remove everything in data except the useLastN most recent items
-  var dataToProject = data.slice(data.length - useLastN);
-  point1 = linearProject(dataToProject, dataToProject.count);
-  point2 = linearProject(dataToProject, 0);
-  // get the gradient
-  var gradient = (point2 - point1) / (dataToProject.count);
-  return gradient > 0;
+function isPositiveChange(x, y) {
+  const gradient = getBestFit(x, y)[0]
+  return gradient > 0
 }
