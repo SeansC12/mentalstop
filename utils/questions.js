fdc3c1e4df05029e9950c2a73f299125f7313739
Xuan Han <tan_xuan_han@s2020.ssts.edu.sg>
2022-08-18 09:50:31 +0800

    Updated line of best fit functions to use list-in-list coordinates

--- utils/questions.js
+++ utils/questions.js
@@ -1,55 +1,58 @@
-function getBestFit(x, y) {
+function getBestFit(coordinates) {
   // This function only generates gradient and y-intercept, see getLineOfBestFit for actual points
-  const dataArrLength = x.length
+  const dataArrLength = coordinates.length;
 
   // Get mean of x and y values of all points
-  let sumX = 0
-  let meanX = 0
-  let sumY = 0
-  let meanY = 0
+  let sumX = 0;
+  let meanX = 0;
+  let sumY = 0;
+  let meanY = 0;
   for (let i = 0; i < dataArrLength; i++) {
-      sumX += x[i]
-      sumY += y[i]
+    const eachCoordinate = coordinates[i];
+    sumX += eachCoordinate[0];
+    sumY += eachCoordinate[1];
   }
-  meanX = sumX / dataArrLength
-  meanY = sumY / dataArrLength
+  meanX = sumX / dataArrLength;
+  meanY = sumY / dataArrLength;
 
   // Get total deviation for x and y values from mean and prepare for finding gradient
-  let totalDevXSquare = 0
-  let totalDevXY = 0
+  let totalDevXSquare = 0;
+  let totalDevXY = 0;
   for (let i = 0; i < dataArrLength; i++) {
-      const eachX = x[i]
-      const eachY = y[i]
-      totalDevXSquare += Math.pow((eachX - meanX), 2)
-      totalDevXY += (eachX - meanX) * (eachY - meanY)
+    const eachCoordinate = coordinates[i];
+    const eachX = eachCoordinate[0];
+    const eachY = eachCoordinate[1];
+    totalDevXSquare += Math.pow((eachX - meanX), 2);
+    totalDevXY += (eachX - meanX) * (eachY - meanY);
   }
 
   // Get gradient
-  const gradient = totalDevXY / totalDevXSquare
+  const gradient = totalDevXY / totalDevXSquare;
 
   // Get y-intercept
-  const interceptY = meanY - gradient * meanX
+  const interceptY = meanY - gradient * meanX;
 
-  return [gradient, interceptY]
+  return [gradient, interceptY];
 }
 
-function getLineOfBestFit(x, y) {
+function getLineOfBestFit(coordinates) {
   // This function only returns y values -- x values will stay the same
-  const dataArrLength = x.length
-  const _a = getBestFit(x, y), gradient = _a[0], interceptY = _a[1]
+  const dataArrLength = coordinates.length;
+  const _a = getBestFit(coordinates), gradient = _a[0], interceptY = _a[1];
 
   // Generate linear regression points
-  let bestFitY = []
+  const bestFitCoordinates = [];
   for (let i = 0; i < dataArrLength; i++) {
-      const eachX = x[i]
-      const newY = gradient * eachX + interceptY
-      bestFitY.push(newY)
+    const eachCoordinate = coordinates[i];
+    const eachX = eachCoordinate[0];
+    const newY = gradient * eachX + interceptY;
+    bestFitCoordinates.push([eachX, newY]);
   }
   
-  return bestFitY
+  return bestFitCoordinates;
 }
 
-function isPositiveChange(x, y) {
-  const gradient = getBestFit(x, y)[0]
-  return gradient > 0
+function isPositiveChange(coordinates) {
+  const gradient = getBestFit(coordinates)[0];
+  return gradient > 0;
 }
