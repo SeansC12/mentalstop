6959b1f29f5696fb6b5058bf90543cc7d6cca88a
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2022-08-20 14:48:22 +0800

    finished up get help professional help page

--- components/GetHelp/ProfessionalSupport/Agency.js
+++ components/GetHelp/ProfessionalSupport/Agency.js
@@ -2,19 +2,28 @@ import React from "react";
 import { useRouter } from "next/router";
 import { motion } from "framer-motion";
 
-function Agency({ details }) {
+function Agency({ details, selectedHelpline, setSelectedHelpline }) {
   const router = useRouter();
   const { name, openingHours, imageLink, link } = details;
   return (
-    <motion.div
-      whileHover={{ opacity: 0.7 }}
-      className="bg-white rounded-2xl flex justify-start items-center flex-col cursor-pointer"
-      onClick={() => router.push(link)}
-    >
-      <img src={imageLink} className="object-contain w-[70%] h-[75%]"></img>
-      <div className="font-lora text-[#577AAF] text-2xl">{name}</div>
-      <div className="font-Inter text-[#577AAF]">{openingHours}</div>
-    </motion.div>
+    <div>
+      {selectedHelpline ? (
+        <div></div>
+      ) : (
+        <motion.div
+          whileHover={{ opacity: 0.7 }}
+          className="bg-white rounded-2xl flex justify-start items-center h-full flex-col cursor-pointer"
+          onClick={() => {
+            // router.push(link);
+            setSelectedHelpline();
+          }}
+        >
+          <img src={imageLink} className="object-contain w-[70%] h-[75%]"></img>
+          <div className="font-lora text-[#577AAF] text-2xl">{name}</div>
+          <div className="font-Inter text-[#577AAF]">{openingHours}</div>
+        </motion.div>
+      )}
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -8,7 +9,7 @@ module.exports = {
     extend: {
       fontFamily: {
         Inter: ["Inter"],
-        lora: ["Lora"]
+        lora: ["Lora"],
       },
     },
   },
