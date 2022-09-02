afe2d0d67ab900877ef5d75fd10a97352bd2e92b
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-09-02 15:07:55 +0800

    questionnare ui needs more work, done apart from that

--- pages/GetHelp/Questions/Results.js
+++ pages/GetHelp/Questions/Results.js
@@ -1,14 +1,104 @@
-import Router from 'next/router'
-import React from 'react'
-import Header from "../../../components/Header/Header"
+import Router from "next/router";
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { motion } from "framer-motion";
+import Header from "../../../components/Header/Header";
+import { useEffect, useState } from "react";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function Results() {
+  const [questionsLogs, setQuestionsLogs] = useState(null);
+
+  let parsedQuestionsLog = null;
+  const date = new Date();
+  const newLog = {
+    date: date.getDate(),
+    month: date.getMonth(),
+    year: date.getFullYear(),
+    score: Router.query.result,
+  };
+  useEffect(() => {
+    const questionsLog = localStorage.getItem("questionsLog");
+    if (questionsLog) {
+      parsedQuestionsLog = JSON.parse(questionsLog);
+      if (
+        parsedQuestionsLog.at(-1).date == newLog.date &&
+        parsedQuestionsLog.at(-1).month == newLog.month &&
+        parsedQuestionsLog.at(-1).year == newLog.year
+      ) {
+        if (
+          confirm(
+            "We have detected that you already wrote in your diary today\nWould you like to update it?"
+          ) == true
+        ) {
+          parsedQuestionsLog.pop();
+          parsedQuestionsLog.push(newLog);
+        }
+      } else {
+        parsedQuestionsLog = [...parsedQuestionsLog, newLog];
+      }
+    } else {
+      parsedQuestionsLog = [newLog];
+    }
+    localStorage.setItem("questionsLog", JSON.stringify(parsedQuestionsLog));
+
+    const labels = parsedQuestionsLog.map((log) => `${log.date}`);
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: "Daily",
+          data: parsedQuestionsLog.map((questionLog) => questionLog.score),
+          borderColor: "rgb(255, 99, 132)",
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
+        },
+        // {
+        //   label: 'Average',
+        //   data: labels.map(() => Math.floor(Math.random() * 10)),
+        //   borderColor: 'rgb(53, 162, 235)',
+        //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        // },
+      ],
+    };
+
+    setQuestionsLogs(data);
+  }, []);
+
   return (
     <div>
       <Header tab="Get Help" />
-      <div>
-        {Router.query.result}
-      </div>
+      <main className="text-center">
+        <div className="p-10">
+          <div className="">
+            <p className="font-bold text-2xl">Today's result: </p>
+            <p>Results: {Router.query.result}/10</p>
+          </div>
+          <div className="my-10 w-[100%] h-0.5 bg-[#aae99a]"></div>
+          <div>
+            <p className="font-bold text-2xl pb-10">Past Week</p>
+            {questionsLogs && <Line data={questionsLogs} />}
+          </div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
--- pages/GetHelp/Questions/index.js
+++ pages/GetHelp/Questions/index.js
@@ -1,70 +1,121 @@
-import { Router, useRouter } from "next/router";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
-// import Header from "../../../components/Header/Header";
+import Header from "../../../components/Header/Header";
 import questions from "../../../public/questions.json";
-// import { SliderWhite } from "../../components/GetHelp/Slider";
 
 let questionScores = 0;
 export default function Questions() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [mcqInput, setMcqInput] = useState(null);
+  const [mcqInput, setMcqInput] = useState([null]);
   let input = 5;
   const router = useRouter();
 
   const handleSubmit = () => {
     questionScores +=
       questions[currentQuestionIndex].answersToPoints[input] ||
-      questions[currentQuestionIndex].answersToPoints[mcqInput];
+      questions[currentQuestionIndex].answersToPoints[mcqInput] ||
+      0;
 
     if (currentQuestionIndex == questions.length - 1) {
       router.push(
-        `/GetHelp/Questions/Results?result=${input}`,
+        `/GetHelp/Questions/Results?result=${questionScores}`,
         "/GetHelp/Questions",
         { shallow: true }
       );
     } else {
       setCurrentQuestionIndex((i) => i + 1);
     }
-    console.log(questions[currentQuestionIndex].answersToPoints[mcqInput]);
   };
 
   return (
     <div>
-      {/* <Header tab="Get Help" /> */}
-      <main>
-        <div className="flex">
-          <div className="">
-            <p>{questions[currentQuestionIndex].question}</p>
-            <p>{questions[currentQuestionIndex].subtext}</p>
-            {questions[currentQuestionIndex].type === "range" ? (
+      <Header tab="Get Help" />
+      <main className="p-10 md:p-16 bg-gradient-to-b from-[#DFFFDC] to-[#E5E5DB] h-screen">
+        <div className="h-screen">
+          <p className="text-4xl">Answer a few questions to determine if you may be at risk of mental health issues</p>
+        </div>
+        <div className="">
+          <p className="font-bold text-4xl ">
+            {questions[currentQuestionIndex].question}
+          </p>
+          <p>{questions[currentQuestionIndex].subtext}</p>
+          {questions[currentQuestionIndex].type === "range" ? (
+            <div className="flex flex-col text-3xl p-10">
+              <div className="grid grid-cols-11 text-center w-full">
+                <p className="ml-0">0</p>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+                <p>4</p>
+                <p>5</p>
+                <p>6</p>
+                <p>7</p>
+                <p>8</p>
+                <p>9</p>
+                <p>10</p>
+              </div>
+              {/* <p className="pr-10">0</p> */}
               <input
+                className="w-full h-2 rounded-lg appearance-none cursor-pointer bg-gray-500"
                 type="range"
                 onChange={(val) => {
-                  input = val;
+                  input = Math.round(val.target.value);
                 }}
                 min={1}
                 max={10}
+                step={0.1}
               ></input>
-            ) : (
-              Object.entries(
-                questions[currentQuestionIndex].answersToPoints
-              ).map((answer) => (
+              {/* <p className="pl-10">10</p> */}
+            </div>
+          ) : (
+            Object.entries(questions[currentQuestionIndex].answersToPoints).map(
+              (answer) => (
                 <button
+                  key={answer}
                   onClick={() => {
                     setMcqInput(answer);
+                    console.log(mcqInput);
+                    console.log(answer);
+                    console.log(mcqInput == answer);
                   }}
                   style={{
-                    backgroundColor: mcqInput === answer ? "blue" : "white",
+                    backgroundColor:
+                      mcqInput[0] == answer[0] ? "blue" : "white",
                   }}
                 >
                   {answer}
                 </button>
-              ))
-            )}
-            <button onClick={handleSubmit}>Submit</button>
-          </div>
+              )
+            )
+          )}
+          <button
+            className="p-3 px-7 rounded-full bg-gradient-to-r from-[rgb(80,80,160)] to-[rgb(40,200,200)] text-xl text-white"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
         </div>
       </main>
     </div>
   );
 }
+
+// .btn-grad {
+//   background-image: linear-gradient(to right, #16A085 0%, #F4D03F  51%, #16A085  100%);
+//   margin: 10px;
+//   padding: 15px 45px;
+//   text-align: center;
+//   text-transform: uppercase;
+//   transition: 0.5s;
+//   background-size: 200% auto;
+//   color: white;
+//   box-shadow: 0 0 20px #eee;
+//   border-radius: 10px;
+//   display: block;
+// }
+
+// .btn-grad:hover {
+//   background-position: right center; /* change the direction of the change here */
+//   color: #fff;
+//   text-decoration: none;
+// }
