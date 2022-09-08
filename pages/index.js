dcdb5fc9f10dc9160f9ba7f30fa6eecfe7ba5e62
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2022-09-08 10:50:39 +0800

    mobile view for homepage fixed

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import "../public/encouragement2.png";
 import "../public/performance.png";
 import "../public/qualityOfLife.png";
 import "../public/relationships.png";
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
 const cardInfos = [
   {
@@ -78,7 +79,7 @@ export default function Home() {
           <p className="mt-3">- Henry David Thoreau</p>
         </div>
         <div className="flex h-[30vh] rounded-2xl p-2 md:p-10 bg-gradient-to-r from-[#D23737] to-[#DC5858] items-center text-white mt-10">
-          <div className="align-top text-lg md:text-4xl w-full leading-normal">
+          <div className="align-top text-lg sm:text-xl md:text-2xl lg:text-4xl w-full leading-normal">
             <strong>
               Need help urgently?
               <br />
@@ -88,38 +89,44 @@ export default function Home() {
           <a href="tel:1-767">
             <img className="h-32" src="callIcon.svg" />
           </a>
-          <img className="h-32" src="callIcon.svg"></img>
         </div>
         <div className="text-center">
           <p className="text-2xl md:text-4xl pt-16 font-bold">
             Explore our options:
           </p>
 
-          <div className="grid md:grid-cols-2 h-fit place-items-center md:w-[80%] gap-7 md:gap-14 m-auto mt-8">
+          <div className="grid md:grid-cols-2 h-fit place-items-center lg:w-[80%] gap-7 md:gap-14 m-auto mt-8">
             {cardInfos.map((cardInfo, key) => (
               <div
                 key={key}
-                className="rounded-2xl w-full h-full text-left p-4 md:p-8 relative"
+                className="rounded-2xl w-full h-full text-left p-4 md:p-8 flex flex-col"
                 style={{ backgroundColor: cardInfo.bgColour }}
               >
-                <p className="text-xl sm:text-2xl xl:text-4xl font-lora">
+                <p className="text-base sm:text-lg md:text-2xl xl:text-4xl font-lora">
                   {cardInfo.title}
                 </p>
-                <div className="absolute bottom-8 w-fit">
-                  <Link href={cardInfo.cardLink}>
-                    <b className="text-sm md:text-xl bg-opacity-10 bg-black cursor-pointer w-fit h-fit py-2 px-6 rounded-full font-Inter">
-                      {cardInfo.buttonLabel}
-                    </b>
-                  </Link>
+                <div className="flex flex-row">
+                  <div className="w-full h-full flex justify-center items-center sm:justify-start sm:items-end">
+                    <Link href={cardInfo.cardLink}>
+                      <div className="text-xs lg:text-xl bg-opacity-10 bg-black cursor-pointer w-max h-max py-2 px-4 font-bold md:py-2 md:px-6 rounded-full font-Inter">
+                        {cardInfo.buttonLabel}
+                      </div>
+                    </Link>
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <img
+                      className="w-full aspect-square"
+                      src={cardInfo.imageLink}
+                    ></img>
+                  </div>
                 </div>
-                <img className="w-32 ml-auto" src={cardInfo.imageLink}></img>
               </div>
             ))}
           </div>
           <div className="text-2xl md:text-4xl pt-16 font-bold mb-10">
             It's OKAY to reach out
           </div>
-          <div className="px-20">
+          <div className="px-2 md:px-16">
             <div className="text-base">
               With the daily struggles from life, it is normal to feel stressed,
               sad or even worried. These emotions are very common, and it
@@ -127,36 +134,43 @@ export default function Home() {
               and struggles. When we feel overwhelmed, we can always reach out.
             </div>
             <div className="w-full flex items-center justify-center">
-              <img src="encouragement1.png" width="150" height="150" />
+              {useWindowDimensions().width > 1200 && (
+                <img src="encouragement1.png" width="150" height="150" />
+              )}
               <iframe
-                width="600"
-                height="355"
                 src="https://www.youtube.com/embed/tYj6oJhZrAw"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
-                className="my-7 mx-16"
+                className="w-full aspect-video my-7 mx-20 md:mx-16"
               />
-              <img src="encouragement2.png" width="200" height="200" />
+              {useWindowDimensions().width > 1200 && (
+                <img src="encouragement2.png" width="200" height="200" />
+              )}
             </div>
             <div className="text-xl font-bold">
               There is nothing to be ashamed of. Everyone goes through hard
               times, do not be afraid to reach out for help
             </div>
+            <div className="w-full flex justify-center items-center">
+              {useWindowDimensions().width < 1200 && (
+                <img src="encouragement2.png" width="200" height="200" />
+              )}
+            </div>
           </div>
-          <div className="my-10 grid place-items-center">
+          <div className="mt-5 mb-10 grid place-items-center">
             <div className="border border-[#1A8C10] rounded w-[100%]"></div>
           </div>
           <div className="text-2xl md:text-4xl font-Inter font-bold mb-10">
             Why is it important to seek for help?
           </div>
-          <div className="grid grid-cols-[25%_75%] grid-rows-1 gap-5">
+          <div className="grid grid-cols-1 grid-rows-[17rem_full] gap-5 md:grid-cols-[25%_75%] md:grid-rows-1">
             <div className="flex flex-col gap-3">
               <div
                 className={
                   selectedImportance === 0
-                    ? "w-full rounded-full h-max bg-white text-lg py-6 cursor-pointer opacity-60 shadow-2xl"
+                    ? "w-full rounded-full h-max bg-yellow-100 text-lg py-6 cursor-pointer shadow-2xl"
                     : "w-full rounded-full h-max bg-white text-lg py-6 cursor-pointer"
                 }
                 onClick={() => setSelectedImportance(0)}
@@ -166,7 +180,7 @@ export default function Home() {
               <div
                 className={
                   selectedImportance === 1
-                    ? "w-full rounded-full h-max bg-white text-lg py-6 cursor-pointer opacity-60 shadow-2xl"
+                    ? "w-full rounded-full h-max bg-yellow-100 text-lg py-6 cursor-pointer shadow-2xl"
                     : "w-full rounded-full h-max bg-white text-lg py-6 cursor-pointer"
                 }
                 onClick={() => setSelectedImportance(1)}
@@ -176,7 +190,7 @@ export default function Home() {
               <div
                 className={
                   selectedImportance === 2
-                    ? "w-full rounded-full h-max bg-white text-lg py-6 cursor-pointer opacity-60 shadow-2xl"
+                    ? "w-full rounded-full h-max bg-yellow-100 text-lg py-6 cursor-pointer shadow-2xl"
                     : "w-full rounded-full h-max bg-white text-lg py-6 cursor-pointer"
                 }
                 onClick={() => setSelectedImportance(2)}
@@ -184,7 +198,7 @@ export default function Home() {
                 Relationships
               </div>
             </div>
-            <div className="w-full h-full rounded-xl bg-white p-4 grid grid-cols-[25%_75%] grid-rows-1">
+            <div className="w-full h-[110%] rounded-xl bg-white p-4 grid grid-cols-[25%_75%] grid-rows-1">
               <div className="flex justify-center items-center">
                 <img
                   src={
