5ce23918d7fd1752e2a1a88160c20c2c1e8f4470
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2022-09-12 17:31:57 +0800

    remove unnecessary comments again

--- components/LearnMore/Modal.js
+++ components/LearnMore/Modal.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
-import "../../public/closeButton.svg"
+import "../../public/closeButton.svg";
 
-function Modal({data, setShowModal}) {
+function Modal({ data, setShowModal }) {
   return (
     <motion.div
       animate={{ opacity: 1 }}
@@ -15,15 +15,15 @@ function Modal({data, setShowModal}) {
         onClick={() => setShowModal(-1)}
       ></div>
 
-      <div className="w-1/2 h-1/2 rounded-md p-5 bg-gray-200 z-50 relative">
+      <div className="w-3/4 md:w-1/2 h-max rounded-md p-5 bg-gray-200 z-50 relative overflow-y-scroll">
         <img
           src="closeButton.svg"
           className="absolute right-2 top-2 cursor-pointer"
           onClick={() => setShowModal(-1)}
         />
         <div>
-          <strong className="text-2xl">{data.name}</strong>
-          <p className="text-lg pt-4">{data.description}</p>
+          <strong className="text-lg sm:text-2xl">{data.name}</strong>
+          <p className="text-base sm:text-lg pt-4">{data.description}</p>
         </div>
       </div>
     </motion.div>
