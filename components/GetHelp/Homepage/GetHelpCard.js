cdc355895b70a8e8577c813833b5cfa6a8c4b9c3
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2022-09-13 20:31:14 +0800

    fixed warning in gethelp

--- components/GetHelp/Homepage/GetHelpCard.js
+++ components/GetHelp/Homepage/GetHelpCard.js
@@ -8,7 +8,7 @@ function GetHelpCard({ data, index }) {
   const colours = ["#DED4D4", "#E2DB98", "#AFE9AA", "#C6DAF9"];
   return (
     <div
-      className={`flex flex-col p-4 md:p-8 text-left bg-slate-500 rounded-2xl font-Inter min-h-56 bg-cover bg-[url("https://images.unsplash.com/photo-1554757387-2a28855c78fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80")]`}
+      className={`flex flex-col p-4 md:p-8 text-left bg-slate-500 rounded-2xl min-h-56 bg-cover bg-[url("https://images.unsplash.com/photo-1554757387-2a28855c78fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80")]`}
     >
       <h1 className="font-normal font-lora text-2xl md:text-4xl">{Header}</h1>
       <h2 className="font-medium font-Inter my-2 md:text-xl">{Copy}</h2>
