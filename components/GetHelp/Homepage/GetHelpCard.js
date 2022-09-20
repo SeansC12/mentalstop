4fc8e6895044fe6c01bcb301bc7b8b5c0af599be
harish-f <harish_ram@s2021.ssts.edu.sg>
2022-09-20 08:52:25 +0800

    standardised colours on gethelp and homepage cards

--- components/GetHelp/Homepage/GetHelpCard.js
+++ components/GetHelp/Homepage/GetHelpCard.js
@@ -6,20 +6,19 @@ function GetHelpCard({ data, index, onClickHandler }) {
   let { Header, Copy, ButtonText, redirectLink } = data;
 
   const buttonColours = [
-    { backgroundColor: "#6be161" },
-    { backgroundColor: "#6aa3f9" },
+    { backgroundColor: "rgb(175, 233, 170)" },
+    { backgroundColor: "rgb(198, 218, 249)" },
   ];
 
   const backgroundColourStyle = [
-    { background: "linear-gradient(to right, #AFE9AA, #c7f4c3)" },
-    { background: "linear-gradient(to right, #C6DAF9, #d2edeb)" },
+    { background: "rgb(202, 234, 195)" },
+    { background: "rgb(211, 233, 233)" },
   ];
 
   return (
     <div
       className="flex flex-col p-4 md:p-8 text-left rounded-2xl min-h-56 shadow-lg bg-cover"
-      style={backgroundColourStyle[index % 2]}
-    >
+      style={backgroundColourStyle[index % 2]}>
       <h1 className="font-normal font-lora text-2xl md:text-4xl">{Header}</h1>
       <h2 className="font-normal font-Inter my-2 md:text-lg">{Copy}</h2>
       <button
@@ -30,9 +29,8 @@ function GetHelpCard({ data, index, onClickHandler }) {
             onClickHandler();
           }
         }}
-        className={`rounded-full shadow-lg px-4 py-2 w-max`}
-        style={buttonColours[index % 2]}
-      >
+        className="rounded-full shadow font-bold px-4 py-2 w-max"
+        style={buttonColours[index % 2]}>
         {ButtonText}
       </button>
     </div>
