47623c3531d6d691003b767bea08c0d504dd8c25
harish-f <harish_ram@s2021.ssts.edu.sg>
2022-10-16 10:24:27 +0800

    added favicona and title

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
+import "../public/mentalStopLogo.png"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MentalStop</title>
+        <link rel="icon" type="image/x-icon" href="mentalStopLogo.png"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
